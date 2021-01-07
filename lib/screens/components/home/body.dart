8935a07ceced281a2b52913806e15caa1620c877
Nurbek <tuzelnurbek@gmail.com>
2021-01-07 22:09:55 +0600

    home page

--- lib/screens/components/home/body.dart
+++ lib/screens/components/home/body.dart
@@ -72,11 +72,78 @@ class HomeBody extends StatelessWidget {
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home page',
-        style: TextStyle(
-          color: Colors.black,
+    List<Map<String, dynamic>> categorys = [
+      {"icon": "assets/icons/flash.svg", "text": "Главная"},
+      {"icon": "assets/icons/flash.svg", "text": "Главная"},
+      {"icon": "assets/icons/flash.svg", "text": "Главная"},
+      {"icon": "assets/icons/flash.svg", "text": "Главная"},
+      {"icon": "assets/icons/flash.svg", "text": "Главная"}
+    ];
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.only(top: 35),
+              width: double.infinity,
+              height: 90,
+              decoration: BoxDecoration(
+                color: Color(0xFF4A3298),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text.rich(
+                TextSpan(text: 'Welcome Тузелбеков Нурбек!'),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontStyle: FontStyle.italic,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ...List.generate(
+                    categorys.length,
+                    (index) => SizedBox(
+                      width: 55,
+                      child: Column(
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                color: Color(0xFFFFECDF),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: IconButton(
+                                icon:
+                                    SvgPicture.asset(categorys[index]["icon"]),
+                                onPressed: () => print(index),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            categorys[index]["text"],
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
