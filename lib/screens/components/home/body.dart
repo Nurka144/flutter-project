216e97ec3f9c1f7ac8f00ec7a838d2a6d4961bb9
Nurbek <tuzelnurbek@gmail.com>
2021-01-22 14:12:45 +0600

    test get token in home screen

--- lib/screens/components/home/body.dart
+++ lib/screens/components/home/body.dart
@@ -1,11 +1,9 @@
 import 'package:app_mobile_test/components/drawer.dart';
 import 'package:app_mobile_test/screens/QRSceen.dart';
-import 'package:app_mobile_test/screens/components/home/bannser-sliter.dart';
-import 'package:app_mobile_test/screens/components/home/home-nav-tabs.dart';
-import 'package:app_mobile_test/screens/components/home/sectionTitle.dart';
 import 'package:app_mobile_test/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:app_mobile_test/screens/components/home/homebody.dart';
 
 class HomeBody extends StatefulWidget {
   HomeBody({Key key}) : super(key: key);
@@ -60,73 +58,6 @@ class _HomeBodyState extends State<HomeBody> {
             width: SizeConfig.defaultSize,
           )
         ],
-      ),
-    );
-  }
-}
-
-class Body extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 35),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Color(0xFF4A3298),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    height: 30,
-                    width: double.infinity,
-                    child: Text.rich(
-                      TextSpan(text: 'Добро пожаловать, Тузелбеков Нурбек!'),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontStyle: FontStyle.italic,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 31,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            HomeNavTabs(),
-            SectionTitle(
-              text: "Последние просмотренные",
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  BannerSlider(),
-                ],
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
