be6ad10e6c0082a874ef679208a9c6d009ca47ae
Nurbek <tuzelnurbek@gmail.com>
2021-02-02 00:25:41 +0600

    home-card

--- lib/screens/components/home/body.dart
+++ lib/screens/components/home/body.dart
@@ -13,52 +13,175 @@ class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.only(bottom: 20.0 * 2.5),
-      height: size.height * 0.2,
-      child: Stack(
+    return SingleChildScrollView(
+      child: Column(
         children: [
           Container(
-            padding: EdgeInsets.only(
-              left: 20,
-              right: 20,
-              bottom: 10,
-            ),
-            height: size.height * 0.2 - 20,
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              ),
-            ),
-            child: Row(
-              children: <Widget>[
-                Text(
-                  'Привет, Нурбек!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+            margin: EdgeInsets.only(bottom: 20.0 * 2.5),
+            height: size.height * 0.2,
+            child: Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    bottom: 10,
+                  ),
+                  height: size.height * 0.2 - 20,
+                  decoration: BoxDecoration(
+                    color: kPrimaryColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        'Привет, Нурбек!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Spacer(),
+                      Text('Logo User'),
+                    ],
                   ),
                 ),
-                Spacer(),
-                Text('Logo User'),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    height: 40,
+                    width: size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: kPrimaryColor.withOpacity(0.23),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            onChanged: (value) {},
+                            decoration: InputDecoration(
+                              hintText: "Поиск",
+                              hintStyle: TextStyle(
+                                color: kPrimaryColor.withOpacity(0.5),
+                              ),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: SvgPicture.asset(
+                            "assets/icons/search.svg",
+                            color: Colors.black,
+                            height: 35,
+                          ),
+                          onPressed: () {
+                            print('search');
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
+          Products(),
+        ],
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+    );
+  }
+}
+
+class Products extends StatelessWidget {
+  const Products({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: double.infinity,
+      child: Wrap(
+        alignment: WrapAlignment.spaceAround,
+        runSpacing: 10,
+        children: [
+          ProductCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            count: 440,
+          ),
+          ProductCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            count: 440,
+          ),
+          ProductCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            count: 440,
+          ),
+          ProductCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            count: 440,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key key,
+    this.image,
+    this.title,
+    this.count,
+    this.country,
+  }) : super(key: key);
+
+  final String title, country, image;
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width * .45,
+      child: Column(
+        children: [
+          Image.asset(image),
+          GestureDetector(
+            onTap: () => {},
             child: Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              height: 40,
-              width: size.width,
+              padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
                 boxShadow: [
                   BoxShadow(
                     offset: Offset(0, 10),
@@ -69,33 +192,33 @@ class _HomeBodyState extends State<HomeBody> {
               ),
               child: Row(
                 children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        hintText: "Поиск",
-                        hintStyle: TextStyle(
-                          color: kPrimaryColor.withOpacity(0.5),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: "$title\n".toUpperCase(),
+                            style: Theme.of(context).textTheme.button),
+                        TextSpan(
+                          text: "$country".toUpperCase(),
+                          style: TextStyle(
+                            color: kPrimaryColor.withOpacity(0.5),
+                          ),
                         ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
+                      ],
                     ),
                   ),
-                  IconButton(
-                    icon: SvgPicture.asset(
-                      "assets/icons/search.svg",
-                      color: Colors.black,
-                      height: 35,
-                    ),
-                    onPressed: () {
-                      print('search');
-                    },
+                  Spacer(),
+                  Text(
+                    '\$$count',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: kPrimaryColor),
                   )
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
