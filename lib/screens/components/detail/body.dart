e9ef0cb0683e5ae15ccf557ec84416059b9e872d
Nurbek <tuzelnurbek@gmail.com>
2021-02-06 23:12:55 +0600

    detailt page

--- lib/screens/components/detail/body.dart
+++ lib/screens/components/detail/body.dart
@@ -12,8 +12,54 @@ class DetailBody extends StatelessWidget {
             child: Stack(
               children: [
                 Container(
-                  height: 500,
-                  decoration: BoxDecoration(color: Colors.white),
+                  margin: EdgeInsets.only(top: 100),
+                  padding: EdgeInsets.all(20),
+                  //height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [Text('asca')],
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Samantha',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.asset("assets/images/image_1.png"),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ],
             ),
